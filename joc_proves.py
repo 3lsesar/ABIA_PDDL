2cd11c927b82c4a7f537ee3c98e4136bd35579da
3lsesar <119434908+3lsesar@users.noreply.github.com>
2023-11-30 11:50:51 +0100

    Joc de proves fins extensio 2

--- joc_proves.py
+++ joc_proves.py
@@ -14,15 +14,41 @@ class Generador:
 
         def __repr__(self):
             return self.name
+        
+        def llegir(self):
+            self.llegit = True
 
         def add_predecesor(self, node):
             self.predecesors.append(node)
 
-        def llegir(self):
-            self.llegit = True
+        def comp_pred(self, book):
+            
+            if self.predecesors == []:
+                return True 
+            else:
+                for elem in self.predecesors: 
+                    if book not in elem.predecesors:
+                        return elem.comp_pred(book)
+                    else:
+                        return False
+            return True
 
+        def comp_paralel(self, book):
+            
+            if book not in self.paralel or self.paralel == []:
+                if self.predecesors == []:
+                    return True
+                
+                elif book in self.predecesors: 
+                    return False
+                
+                else:
+                    for elem in self.predecesors:  
+                        return elem.comp_pred(book)          
+            else:
+                return False
+            return True
 
-        
     def __init__(self, nom = 'prova', llibres = 1, llegits = 0, objectius = 1, predecesors = 0, pararels = 0):
         self.archivo = nom
         self.num_llibres = llibres
@@ -37,6 +63,7 @@ class Generador:
              self.generador_objectes(archivo)
              self.generador_mesos(archivo)
              self.generador_llegits(archivo)
+             self.generador_predecesors(archivo)
 
 
     def generador_objectes(self,archivo):
@@ -49,7 +76,7 @@ class Generador:
             character = chr(i)
             archivo.write(character)
             archivo.write(" ")
-            self.llibres.append(Node(character))
+            self.llibres.append(self.Node(character))
         archivo.write("- llibre) \n")
     
     def generador_mesos(self,archivo):
@@ -96,18 +123,38 @@ class Generador:
             archivo.write(") \n")
             archivo.write("\n")
         archivo.write("\n")
-    
+         
     def generador_predecesors(self,archivo):
-        for i in range(self.num_predecesors):
+        for _ in range(self.num_predecesors):
             llibre = random.choice(self.llibres)
             predecesor = random.choice(self.llibres)
-            while predecesor == llibre:
+            while (predecesor.comp_pred(llibre) == False) or predecesor == llibre or predecesor in llibre.predecesors:
                 predecesor = random.choice(self.llibres)
             llibre.add_predecesor(predecesor)
             archivo.write("  (predecessor ")
+            archivo.write(predecesor.name)
+            archivo.write(" ")
+            archivo.write(llibre.name)
+            archivo.write(") \n")
+        archivo.write("\n")
+
+    def generador_paralels(self,archivo):
+        for _ in range(self.num_paralels):
+            llibre = random.choice(self.llibres)
+            paralel = random.choice(self.llibres)
+            while llibre.comp_paralel(paralel) == False or paralel == llibre:
+                paralel = random.choice(self.llibres)
+            llibre.paralel.append(paralel)
+            paralel.paralel.append(llibre)
+            archivo.write("  (paralel ")
             archivo.write(llibre.name)
             archivo.write(" ")
-            archivo.write(predecesor.name)
+            archivo.write(paralel.name)
+            archivo.write(") \n")
+            archivo.write("  (paralel ")
+            archivo.write(paralel.name)
+            archivo.write(" ")
+            archivo.write(llibre.name)
             archivo.write(") \n")
         archivo.write("\n")
 
